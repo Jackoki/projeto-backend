9da14b914643b26477ce55821d65ba0705d601d7
Pedro-Landgraf <143462985+Pedro-Landgraf@users.noreply.github.com>
2024-12-03 10:14:41 -0300

    Update usersController.js

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -17,11 +17,21 @@ const getUserById = (req, res) => {
 }
 
 const createUser = (req, res) => {
+    const User = createUser(req.body);
+    User.isAdmin = false;
+    if (!User.name || !User.email || !User.user || !User.password) {
+        return res.status(400).json({ message: 'Todos os campos são obrigatórios' });
+    }
     res.status(200).json(users)
 }
 
 
 const createUserAdm = (req, res) => {
+    const User = createUser(req.body);
+    User.isAdmin = true;
+    if (!User.name || !User.email || !User.user || !User.password) {
+        return res.status(400).json({ message: 'Todos os campos são obrigatórios' });
+    }
     res.status(200).json(users)
 }
 
@@ -42,6 +52,16 @@ const updateUserAdm = (req, res) => {
 
 
 const deleteUser = (req, res) => {
+    const deletedUser = deleteUser(parseInt(req.params.id, 10));
+    
+    if(!deletedUser){
+        return res.status(404).json({ message: 'Usuário não encontrado.' });
+    }
+
+    if(deletedUser.isAdmin){
+        return res.status(404).json({ message: 'Admins não podem ser deletados' });
+    }
+    
     res.status(200).json(users)
 }
 
